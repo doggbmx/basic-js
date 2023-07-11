17bbe8190278b697f8f5034a76f91729c5667d23
doggbmx <manuelorticosta@gmail.com>
2023-07-10 22:15:58 -0400

    class 3

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const h1 = document.querySelector("h1");
 const p = document.querySelector("p");
 const parrafito = document.querySelector(".parrafito");
 const pid = document.querySelector("#pid");
+const input = document.querySelector("input");
 
 console.log({
   h1,
@@ -9,3 +10,31 @@ console.log({
   parrafito,
   pid,
 });
+
+// this is how you can change the text of an element
+// or even add more HTML tags inside
+h1.innerHTML = "perrito <br> feo";
+
+// obtain a attribute
+console.log(h1.getAttribute("coso"));
+// change a attribute
+h1.setAttribute("coso", "nuevo valor");
+console.log(h1.getAttribute("coso"));
+
+// add a class atributte
+h1.classList.add("nueva-clase");
+// remove a class atributte
+h1.classList.remove("nueva-clase");
+// toggle a class atributte
+h1.classList.toggle("nueva-clase");
+// check if a class atributte exists
+console.log(h1.classList.contains("nueva-clase"));
+
+input.value = "nuevo valor";
+
+// create a new element
+const newElement = document.createElement("img");
+console.log(newElement);
+newElement.setAttribute("src", "https://picsum.photos/200");
+
+pid.append(newElement);
