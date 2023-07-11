5051837989620faf48806c2fe845b6174167dd05
doggbmx <manuelorticosta@gmail.com>
2023-07-10 22:04:11 -0400

    class 2

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+const h1 = document.querySelector("h1");
+const p = document.querySelector("p");
+const parrafito = document.querySelector(".parrafito");
+const pid = document.querySelector("#pid");
+
+console.log({
+  h1,
+  p,
+  parrafito,
+  pid,
+});
